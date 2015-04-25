455b68eb7ebf2be802ed08eae38bf71fc13a2351
Timothy Gu <timothygu99@gmail.com>
2015-04-25 12:05:57 -0700

    Better legend

--- chart.js
+++ chart.js
@@ -2,6 +2,7 @@ Chart.defaults.global.responsive = true
 Chart.defaults.global.multiTooltipTemplate = '<%= value.toFixed(2) %>'
 //Chart.defaults.global.maintainAspectRatio = false
 Chart.defaults.Line.bezierCurveTension = 0.1
+Chart.defaults.Line.legendTemplate = '<ul class="list-inline legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
 
 function makeChart (id, datasets) {
   var e = document.getElementById(id)
