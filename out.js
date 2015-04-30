5cf1af6471371874d3d271e687431fb47f086ada
Timothy Gu <timothygu99@gmail.com>
2015-04-29 17:57:33 -0700

    Fancy

--- out.js
+++ out.js
@@ -33,11 +33,6 @@ module.exports.revert = function revertToOrig (datasets) {
 var Chart = require('chart.js')
   , raw = require('./data')
 
-Chart.defaults.global.responsive = true
-Chart.defaults.global.multiTooltipTemplate = '<%= value.toFixed(2) %>'
-//Chart.defaults.global.maintainAspectRatio = false
-Chart.defaults.Line.bezierCurveTension = 0.1
-Chart.defaults.Line.legendTemplate = '<ul class="list-inline legend" id="legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
 
 module.exports = function makeChart (id, datasets) {
   var e = document.getElementById(id)
@@ -46,6 +41,12 @@ module.exports = function makeChart (id, datasets) {
   var chart = new Chart(ctx).Line({
     labels: raw.courses
   , datasets: raw
+  },
+  { responsive: true
+  , multiTooltipTemplate: '<%= value.toFixed(2) %>'
+//, maintainAspectRatio: false
+  , bezierCurveTension: 0.1
+  , legendTemplate: '<ul class="list-inline legend" id="legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
   })
 
   e.insertAdjacentHTML('afterend', chart.generateLegend())
